6e9b69ccfa52dde1fe7fad4f53ddf52ee659d0d9
daande01 <daniel.evert.andersson@gmail.com>
2026-01-12 10:15:41 +0100

    metoder

---
+++ src/metoder/Startm.java
@@ -0,0 +1,33 @@
+package metoder;
+
+import java.util.Scanner;
+
+public class Startm {
+
+public static void main(String[] args) {
+    
+
+    
+Startm.helloWorld();
+Startm.checkInput(11);
+}
+
+public static void helloWorld(){
+
+    System.out.println("hello world");
+}
+public static void checkInput(int tal){
+
+    if(tal>10){
+
+        System.out.println("input ok ");
+    }else{
+        System.out.println("input ! ok ");
+    }
+
+}
+
+
+
+
+}
